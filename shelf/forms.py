506913e4a5d3df4605361ce2a29e21694d159db5
teacher PH2021 <postnikov_s@teacher.itstep.by>
2022-09-06 22:07:15 +0300

    TODO pic

--- shelf/forms.py
+++ shelf/forms.py
@@ -6,13 +6,13 @@ class AuthorForm(forms.ModelForm):
     class Meta:
         model = Author
         #fields = '__all__'
-        exclude = ['pic']
+        exclude = ['pic'] #TODO fix pic
 
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         #fields = '__all__'
-        exclude = ['pic']
+        exclude = ['pic'] # TODO fix pic
 
 class ShelfForm(forms.ModelForm):
     class Meta:
